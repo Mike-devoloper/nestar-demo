a18fd0bf25a1518626333ee315b8f6bb45ba3d02
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-12-28 20:28:05 +0900

    feat integration meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     <Head>
+        <meta charSet="UTF-8"/>
+        <meta name="title" content="Nestar demo"/>
+        <meta name="robots" content="index, follow"/>
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg"/>
+        {/*SEO */}
+        <meta name="keyword" content={"nestar, nestar.uz, devex mern, mern nestjs fullstack "}/>
+        <meta name="description" content={
+          "Buy and Sell properties anywhere anytime in South Korea |" +
+          "Покупайте и продавайте недвижимость в любое время и в любом месте в Южной Корее. |" +
+          "언제 어디서나 대한민국에서 부동산을 사고파세요."
+        }/>
+      </Head>
       <body>
         <Main />
         <NextScript />
