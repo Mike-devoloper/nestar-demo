73ee189608a3e2c727f13e7874f3f6de42dfcede
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-12-30 03:03:40 +0900

    feat: publish main layout

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,6 +1,9 @@
 import { brown, green } from "@mui/material/colors"
 import { Stack } from "@mui/system"
 import Head from "next/head"
+import HeaderFilter from "../HeaderFilter"
+import Footer from "./Footer"
+import Top from "./Top"
 
 const withLayoutMain = (Component: any) => {
     return (props: any) => {
@@ -8,13 +11,22 @@ const withLayoutMain = (Component: any) => {
             <>
        <Head><title>Nestar</title></Head>
        <Stack id="pc-wrap">
-       <Stack  sx={{background: green[300]}}>Header</Stack>
+       <Stack id={"top"}>
+        <Top/>
+       </Stack>
+       <Stack className={"header-main"}>
+            <Stack className={"container"}>
+                <HeaderFilter/>
+            </Stack>
+       </Stack>
 
         <Stack id="main">
             <Component {...props}/>
         </Stack>
 
-       <Stack sx={{background: brown[300]}}>Footer</Stack>
+       <Stack id={"footer"}>
+        <Footer/>
+       </Stack>
        </Stack>
        </>
         )
